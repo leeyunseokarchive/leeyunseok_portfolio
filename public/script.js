cc894273e6a78c149c8d3913d602cae232692e16
leeyunseok <dbstjr3576@gmail.com>
2025-07-15 12:13:06 +0900

    feat(home): 홈화면 구현 및 파이어베이스 연동
    - 홈화면 구현
    - 파이어베이스 연동
    - 호스팅

---
+++ public/script.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDMmT2SkLGNevAKRCDADLjSTd0TcC8pfo",
+  authDomain: "demoweb-63168.firebaseapp.com",
+  projectId: "demoweb-63168",
+  storageBucket: "demoweb-63168.firebasestorage.app",
+  messagingSenderId: "223776136595",
+  appId: "1:223776136595:web:438a4aaaa6268049a19c66"
+};
+
+const app = initializeApp(firebaseConfig);
